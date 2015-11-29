029d35b548d6a96976a721bf46bf63e770f020fb
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-11-29 08:39:28 -0600

    Initial Commit

---
+++ Parse Object/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parse Object
+//
+//  Created by Walter Gonzalez Domenzain on 29/11/15.
+//  Copyright © 2015 Smartplace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Parse Object/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Parse Object
+//
+//  Created by Walter Gonzalez Domenzain on 29/11/15.
+//  Copyright © 2015 Smartplace. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
